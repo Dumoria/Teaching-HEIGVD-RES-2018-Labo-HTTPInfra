c5b0340f1449e6c56cf574421b13da55cd29b241
Cloud <benjamin.thomas@heig-vd.ch>
2018-06-09 23:51:11 +0200

    ...

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,18 +1,67 @@
+//----------Var declarations---------
+
+//Random name generator
 var Chance = require('chance');
 var chance = new Chance();
 
+//Server HTTP
 var express = require('express');
 var app = express();
 
+//HTTP request parser
+const bodyParser = require("body-parser");
+
+//Parser JSON to XML
+var xml = require('xml');
+var xmlString = xml(xmlObject, options);
+
+//Parser JSON to HTML
+var json2html = require('html2json').json2html;
+
+
+//Var time
+var seconde;
+var minute;
+
+//-----------Server gestion----------
+
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+    console.log(req.body.user.name)
+});
+
 app.get('/', function(req, res){
+	
 	res.send(generateStudents());
 });
 
+/*
+app.get('/', function(req, res){
+	res.send(generateStudents());
+});
+*/
 
 app.listen(3000, function(){
 	console.log('Accepting HTTP requests on port 3000');
 });
 
+function updateTime(){
+	seconde++;
+	if(!(seconde % 60)){
+		minute++;
+		seconde = 0;
+	}
+}
+
+var timer = setInterval(updateTime(), 1000);
+
+function setTime(){
+
+}
+
+
+//--------Random name generator-----------
 function generateStudents(){
 	var numberOfStudents = chance.integer({
 		min : 0,
