68036045afe13911c5d0ccb35416dd4fc1eca331
Cloud <benjamin.thomas@heig-vd.ch>
2018-06-11 23:20:22 +0200

    Mise à jour index.js

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,18 +1,48 @@
+//Generateur aleatoire
 var Chance = require('chance');
 var chance = new Chance();
 
+//Construction d'une app express acceptant les requêtes HTTP
 var express = require('express');
 var app = express();
 
+//Pour les requêtes get avec uniquement un / dans l'url
 app.get('/', function(req, res){
-	res.send(generateStudents());
+	res.send(generateMatrix());
+	//res.send(generateStudents()); //uncomment for webcast's dynamic content
 });
 
-
+//Se mettre à l'écoute sur le port 3000
 app.listen(3000, function(){
 	console.log('Accepting HTTP requests on port 3000');
 });
 
+//Fonction pour contenu dynamique custom
+function generateMatrix(){
+	
+	var size = chance.integer({
+		min : 0,
+		max : 10
+	});
+	
+	var matrix = [];
+	for(var line = 0; line < size; ++line){
+		
+		matrix[line] = new Array(size);
+		
+		for(var column = 0; column < size; ++column){
+			matrix[line][column] = chance.integer({
+				min : 0,
+				max : 1
+			});
+		}
+	}
+	console.log(matrix);
+	return matrix;
+}
+
+
+//Fonction pour contenu dynamique webcast
 function generateStudents(){
 	var numberOfStudents = chance.integer({
 		min : 0,
